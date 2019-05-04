2c7dbedcd216b0d93562f253f182650e13e2b13b
SantosHM <dosantoshm@gmail.com>
2019-05-04 15:57:21 -0500

    Creando un menu respondive con bt

--- header.php
+++ header.php
@@ -10,13 +10,22 @@
 		<nav class="navegacion">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-4">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
 						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" class="img-responsive">
 					</div>
-					<div class="col-md-8">
+					<div class="navbar-right">
 						<?php  
 						wp_nav_menu( array( 
-							'theme_location' => 'menu_principal' 
+							'theme_location' => 'menu_principal',
+							'container_id' => 'navbar',
+							'container_class' => 'collapse navbar-collapse',
+							'menu_class' => 'nav navbar-nav navbar-right' 
 						) );
 						?>
 					</div>
